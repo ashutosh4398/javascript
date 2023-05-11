86e6d4362da9cc78dfe666be3fdf3962053bbcad
ashutosh4398 <ashutoshdhondkar@gmail.com>
2023-05-11 19:56:38 +0530

    assignment basic

---
+++ assignment/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+const userInput = 10;
+let result;
+
+result = 18 + userInput;
+
+result -= 10;
+result *= 4;
+result /= 10;
+
+alert(result);
+alert(userInput);
---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+// dummy value as of now
+currentResult += 10;
+currentResultDescription = defaultResult + ' + 10';
+
+outputResult(currentResult, currentResultDescription);
