0325ee8315890941d8a6d8d0d2ff51c809934f1b
ashutosh4398 <ashutoshdhondkar@gmail.com>
2023-05-12 11:17:09 +0530

    unconventional calculator done

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,13 +1,52 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteLogOutput(operator, calcBefore, newNumber) {
+    const description = `${calcBefore} ${operator} ${newNumber}`;
+    // currentResult is global variable
+    outputResult(currentResult, description);
+}
+
 function addNumbers() {
     // function to add numbers
-    currentResult = currentResult + parseFloat(userInput.value);
-    outputResult(currentResult, '');
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteLogOutput("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+    // function to subtract numbers
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteLogOutput("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+    // function to multiply numbers
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteLogOutput("*", initialResult, enteredNumber);
+}
+
+function divide() {
+    // function to divide numbers
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteLogOutput("/", initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', addNumbers);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 // currentResultDescription = defaultResult + ' + 10';
 
