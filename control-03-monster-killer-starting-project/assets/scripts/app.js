e259e9762165e3ddd2752ba77595cc773c0fa173
ashutosh4398 <ashutoshdhondkar@gmail.com>
2023-05-16 21:12:12 +0530

    strong attack functionality added

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,30 +1,65 @@
 const PLAYER_ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
 
+const STRONG_ATTACK_VALUE = 17;
+
 let chosenMaxLife = 100; // need to rethink of const instead of let;
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
+const PLAYER_ATTACK_MODES = { NORMAL_ATTACK: 0, STRONG_ATTACK: 1 };
+
+
+function playerAttackValue(mode) {
+    if (mode === PLAYER_ATTACK_MODES.NORMAL_ATTACK) {
+        return PLAYER_ATTACK_VALUE;
+    } else if( mode === PLAYER_ATTACK_MODES.STRONG_ATTACK ) {
+        return STRONG_ATTACK_VALUE
+    } else {
+        throw "Invalid mode passed";
+    }
+}
+
+function attackMonster(mode) { 
+    const attackValue = playerAttackValue(mode);
+    const monsterDamage = dealMonsterDamage(attackValue);
+    currentMonsterHealth -= monsterDamage;
+}
+
+function attackPlayer() { 
+    // monster causes damage to player with force of MONSTER_ATTACK_VALUE
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+}
+
 
 function attackHandler() {
-    // player causes damage to monster with force = PLAYER_ATTACK_VALUE
-    const monsterDamage = dealMonsterDamage(PLAYER_ATTACK_VALUE);
-    currentMonsterHealth -= monsterDamage;
+    attackMonster(PLAYER_ATTACK_MODES.NORMAL_ATTACK);
 
     if (currentMonsterHealth <= 0) {
         alert('You won!');
         return;
     }
-
-    // monster causes damage to player with force of MONSTER_ATTACK_VALUE
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth -= playerDamage;
+    attackPlayer()
     if (currentPlayerHealth <= 0) {
         alert('You Lost!');
         return;
     }
-    
+}
+
+function strongAttackHandler() {
+    attackMonster(PLAYER_ATTACK_MODES.STRONG_ATTACK);
+    if (currentMonsterHealth <= 0) {
+        alert('You won!');
+        return;
+    }
+    attackPlayer()
+    if (currentPlayerHealth <= 0) {
+        alert('You Lost!');
+        return;
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
