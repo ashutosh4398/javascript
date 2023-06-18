9fa7c6ebaf36b31560d6be541666a813e258ee5b
ashutosh4398 <ashutoshdhondkar@gmail.com>
2023-06-18 22:14:41 +0530

    class based implementation

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,41 +1,78 @@
-const productList = {
-  products: [
-    {
-      title: "A Pillow",
-      imageUrl: "https://media.istockphoto.com/id/899226398/photo/pillow-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=5pJMfNPtyiZYC7ykDmhi_GEYwXmgTvc8yZp_TSGgt_Q=",
-      price:19.99,
-      description: "A soft pillow",
-    },
-    {
-      title: "A Carpet",
-      imageUrl: "https://t4.ftcdn.net/jpg/00/89/76/09/360_F_89760942_RmpjUzGtDcERW1rlkNaifMr58NCVu7YB.jpg",
-      price: 89,
-      description: "A carpet which you like or not!",
-    },
-  ],
+// convention is to use camel case for class name
+// classes do not replace objects, we create objects using classes
+class Product {
+  // defining class fields
+  // title = "DEFAULT TITLE";
+  // imageUrl;
+  // price;
+  // description;
+  // this refers to instance of class which is created using new keyword
+  constructor(title, imageUrl, price, description) {
+    // class properties
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log("Adding product to cart", this.product);
+  }
+
+  render() {
+    const productEl = document.createElement("li");
+    productEl.className = "product-item";
+    productEl.innerHTML = `
+                    <div>
+                        <img src='${this.product.imageUrl}' alt='${this.product.title}'>
+                        <div class="product-item__content">
+                            <h2>${this.product.title}</h2>
+                            <h3>\$ ${this.product.price}</h3>
+                            <p>${this.product.description}</p>
+                            <button>Add to cart!</button>
+                        </div>
+                    </div>
+                `;
+    const addCartBtn = productEl.querySelector("button");
+    addCartBtn.addEventListener("click", this.addToCart.bind(this));
+    return productEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      "A Pillow",
+      "https://media.istockphoto.com/id/899226398/photo/pillow-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=5pJMfNPtyiZYC7ykDmhi_GEYwXmgTvc8yZp_TSGgt_Q=",
+      19.99,
+      "A soft pillow"
+    ),
+    new Product(
+      "A Carpet",
+      "https://t4.ftcdn.net/jpg/00/89/76/09/360_F_89760942_RmpjUzGtDcERW1rlkNaifMr58NCVu7YB.jpg",
+      89,
+      "A carpet which you like or not!"
+    ),
+  ];
 
   render() {
     const renderHook = document.getElementById("app");
     const prodListEl = document.createElement("ul");
     prodListEl.className = "product-list";
     for (const prod of this.products) {
-      const productEl = document.createElement("li");
-      productEl.className = "product-item";
-      productEl.innerHTML = `
-                <div>
-                    <img src='${prod.imageUrl}' alt='${prod.title}'>
-                    <div class="product-item__content">
-                        <h2>${prod.title}</h2>
-                        <h3>\$ ${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button>Add to cart!</button>
-                    </div>
-                </div>
-            `;
-      prodListEl.append(productEl);
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodListEl.append(prodEl);
     }
     renderHook.append(prodListEl);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
