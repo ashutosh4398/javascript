f6f8a39095308797c365dd69ddaecb098917e6f7
ashutosh4398 <ashutoshdhondkar@gmail.com>
2023-07-19 02:41:32 +0530

    pure functions

---
+++ adv-fn-01-starting-setup/app.js
@@ -0,0 +1,30 @@
+// pure function without side-effects
+const add = (num1, num2) => {
+    return (num1 + num2);
+}
+
+console.log(add(1,4)); // 5
+console.log(add(12,15)); // 27
+
+
+// impure function
+// as the output changes even when the inputs are same
+const addRandom = (num) => {
+    return num + Math.random();
+}
+
+console.log(addRandom(10));
+console.log(addRandom(10));
+console.log(addRandom(10));
+
+
+// impure function
+// causing side effects
+const previousResult = 0;
+const addNumbers = (num1, num2) => {
+    const sum = num1 + num2;
+    previousResult = sum; // modifies something outside the function => causing SIDE EFFECTS
+    return sum
+}
+
+// FACTORY FUNCTIONS
