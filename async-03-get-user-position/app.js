be65b680815f7dfed6e8ba57d180da3969332a3f
ashutosh4398 <ashutoshdhondkar@gmail.com>
2023-07-26 22:04:53 +0530

    explored promises

--- async-03-get-user-position/app.js
+++ async-03-get-user-position/app.js
@@ -1,19 +1,72 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+
+// promises have thress states,
+// RESOLVED
+// PENDING -> PUSHED TO BROWSER THREAD FOR EXECUTING
+// REJECTED
+// SETTLED => IF THERE IS NO MORE .THEN BLOCKS, PROMISE ENTERS IN THIS STATE
+// we can use .finally() block to clean the mess
+
+const setTimer = duration => {
+  const promise = new Promise((resolve, reject) => {
+    // this function is called immediately as soon as promise is instantiated
+    // async code...
+    setTimeout(() => {
+      // mark promise object internally as resolved
+      resolve(`WAITED FOR ${duration/1000} SECS`);
+    },duration);
+  });
+  return promise;
+}
+
+
+const getPosition = (opts={}) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (successResp) => {
+        resolve(successResp);
+      }, 
+      (errorResp) => {
+        reject(errorResp);
+      },
+      opts
+    )
+  });
+  return promise;
+}
+
 function trackUserHandler() {
   let i = 10
-  navigator.geolocation.getCurrentPosition(
-    posData => {
-      setTimeout(() => {
-        console.log(posData);
-        console.log(i)
-      },2000)
-    },
-    error => {
-      console.log(error);
-    }
-  );
+  let prom;
+  let positionDetails; // leveraging closures
+  prom = getPosition()
+    .then(success => {
+      positionDetails = success
+      console.log(prom);
+      return setTimer(2000);
+    })
+    .then(timerResp => {
+      console.log(prom);
+      console.log(timerResp);
+      console.log(positionDetails);
+      return "DONE WITH SUCCESSFUL EXECUTION";
+    })
+    .then(resp => {
+      console.log(resp)
+      return "HEHEHE"; // RETURNS PROMISE WRAPPER
+    })
+    .catch(err => {
+      // catches any exception raised in above chanining
+      // the other .then are skipped!
+      console.log(err);
+    })
+    .finally(()=> {
+      console.log(positionDetails);
+      console.log("SETTLED")
+    });
+  
   setTimeout(() => {
     console.log("EVEN THOUGH THE TIMER IS 0, IT IS PUSHED TO MESSAGE QUEUE AND HENCE THIS LINE IS EXECUTED LATER!");
   },0)
